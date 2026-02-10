ccb1d0b275e7e24ce55a70e51a7113e4b51816f4
Stanley F <stanley.foerster@gmail.com>
2026-02-10 08:59:18 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
     ],
     test_suite='nose.collector',
     install_requires=['scipy>=0.11', 'numpy>=1.6'],
-    setup_requires=['nose>=1.0', 'scipy>=0.11', 'numpy>=1.6'],
-    tests_require=['nose>=1.0', 'scipy>=0.11', 'numpy>=1.6'],
+    setup_requires=['scipy>=0.11', 'numpy>=1.6'],
+    tests_require=['scipy>=0.11', 'numpy>=1.6'],
 )
